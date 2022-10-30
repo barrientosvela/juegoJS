8a8d8d0a68a8e7a2a4eef96bb574b1df4b9d0a04
josevillista <jbarrientosvela@gmail.com>
2022-10-30 23:08:43 +0100

    Movimiento con animaciones

--- js/script.js
+++ js/script.js
@@ -1,35 +1,95 @@
 
+
 var personaje = document.getElementById("personaje");
 
-//cuando se detecta el click le aniade una class al personaje
-document.addEventListener("keypress", function () {
-    if (event.key === " "){
-        personaje.classList.add("salto");
-    }         
-});
-//cuando se detecta el fin de la animacion borra una class al personaje
-personaje.addEventListener('animationend', () => {
-    personaje.classList.remove("salto");
-})
-//cuando se detecta el evento le aniade una class al personaje
-document.addEventListener("keypress", function () {
+//animacion con javascript de movimiento a la derecha
+function movDch() {
+    let id = null;
+    let pos = personaje.offsetLeft;
+    let posM = personaje.offsetLeft + 35;
+    document.getElementById("personajeIni").style.display = 'block';
+    document.getElementById("personajeIzq").style.display = 'none';
+    id = setInterval(frame, 10);
+    function frame() {
+        if (pos <= posM && pos <= window.innerWidth - 200) {
+            pos = pos + 5;
+            personaje.style.left = pos + "px";
+        } else {
+            clearInterval(id);
+        }
+    }
+}
+//animacion con javascript de movimiento a la izquierda
+function movIzq() {
+    let id = null;
+    let pos = personaje.offsetLeft;
+    let posM = personaje.offsetLeft - 35;
+    document.getElementById("personajeIni").style.display = 'none';
+    document.getElementById("personajeIzq").style.display = 'block';
+    id = setInterval(frame, 10);
+    function frame() {
+        if (pos >= posM && pos >= 10) {
+            pos = pos - 5;
+            personaje.style.left = pos + "px";
+        } else {
+            clearInterval(id);
+        }
+    }
+}
+//animacion en css de salto del personaje
+function saltar() {
+    //cuando se detecta el espacio le añade una class al personaje
+    personaje.classList.add("salto");
+    //cuando se detecta el fin de la animacion borra una class al personaje
+    personaje.addEventListener('animationend', () => {
+        personaje.classList.remove("salto");
+    })
+}
+//animacion con javascript de agacharse
+/*
+function agachar() {
+    document.getElementById("personajeIni").style.display = 'none';
+    document.getElementById("personajeAgachado").style.display = 'block';
+    document.addEventListener("keyup", function () {
+        if (event.key == "s") {
+            document.getElementById("personajeIni").style.display = 'block';
+            document.getElementById("personajeAgachado").style.display = 'none';
+        }
+    });
+}
+*/
+function fin(){
+    
+}
+function puzle() {
+
+}
+
+//cuando se detecta el evento de pulsar una tecla llama a una función
+document.addEventListener("keydown", function () {
     switch (event.key) {
         case "w":
-            alert("Tecla w")
-            break;
         case "W":
-            console.log()
+        case " ":
+            saltar();
             break;
-
         case "d":
         case "D":
-            personaje.style.left = (personaje.offsetLeft + 10) + "px";
+            movDch();
             break;
         case "a":
         case "A":
-            personaje.style.left = (personaje.offsetLeft - 10) + "px";
+            movIzq();
             break;
-
+        case "s":
+        case "S":
+            agachar();
+            break;
+        case "e":
+        case "E":
+            if (personaje.offsetLeft >= window.innerWidth - 100) {
+                puzle();
+            }
     }
 });
 
